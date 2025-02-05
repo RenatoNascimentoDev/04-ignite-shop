4c45ce3376cda524522cc3bedaf63956a019de5a
Renato Nascimento <90713455+RenatoNascimentoDev@users.noreply.github.com>
2025-02-05 15:31:54 +0000

    Produto & Checkout - Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,29 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,98</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
+          repellendus molestias totam esse quis saepe, fuga eius. Dicta, soluta
+          similique accusamus quas animi nam aspernatur deleniti excepturi sint
+          illo fuga.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
