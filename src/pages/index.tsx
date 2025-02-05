deb58fc9ba415f35aecb7845077752532973ae52
Renato Nascimento <90713455+RenatoNascimentoDev@users.noreply.github.com>
2025-02-05 14:57:12 +0000

    Produto & Checkout - Navegação via Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
-import { useKeenSlider } from "keen-slider/react";
-import { HomeContainer, Product } from "../styles/pages/home";
-import "keen-slider/keen-slider.min.css";
-import { stripe } from "../lib/stripe";
 import { GetStaticProps } from "next";
+import Link from "next/link";
+
+import { useKeenSlider } from "keen-slider/react";
+
+import { stripe } from "../lib/stripe";
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import "keen-slider/keen-slider.min.css";
 import Stripe from "stripe";
 
 interface HomeProps {
@@ -27,14 +31,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         );
       })}
     </HomeContainer>
@@ -64,6 +70,6 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       products,
     },
-    revalidate: 60 * 60 * 2,
+    revalidate: 60 * 60 * 2, // 2 hours,
   };
 };
